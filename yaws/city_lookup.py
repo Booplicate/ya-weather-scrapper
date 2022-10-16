4a83e6b03327f5ce7f3510326554084422dfa86c
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-10-16 11:23:02 +0300

    fixes/cleanup

--- yaws/city_lookup.py
+++ yaws/city_lookup.py
@@ -8,13 +8,13 @@ import csv
 from collections import defaultdict
 
 
-CITY_DB_PATH = "./city_coords.csv"
+CITY_DB_PATH = "./yaws/city_coords.csv"
 
 
 
 class CityData():
     """
-    Class to represend information about a city
+    Class to represent information about a city
     """
     CITIES_MAP: defaultdict[str, list[CityData]] = defaultdict(list)
 
@@ -34,13 +34,20 @@ class CityData():
 
     __repr__ = __str__
 
+    @staticmethod
+    def __name_to_key(name: str) -> str:
+        """
+        Converts a city name to a key to use in the inner map
+        """
+        return name.replace(" ", "").lower()
+
     @classmethod
     def register_city(cls, *args, **kwargs):
         """
         Registers a new city
         """
         city = CityData(*args, **kwargs)
-        key = city.name.replace(" ", "").lower()
+        key = cls.__name_to_key(city.name)
         cls.CITIES_MAP[key].append(city)
 
     @classmethod
@@ -48,17 +55,20 @@ class CityData():
         """
         Returns data for the cities with the given name
         """
-        return cls.CITIES_MAP.get(name.lower(), [])
+        return cls.CITIES_MAP.get(cls.__name_to_key(name), [])
 
 
 def _sanitize_coord(coord: str) -> float:
     return float(coord.replace(",", "."))
 
-def init():
+def init(db_path: str|None = None):
     """
     Inits city data from the csv db, must be called first
     """
-    with open(CITY_DB_PATH, encoding="utf-8", newline="") as city_db:
+    if db_path is None:
+        db_path = CITY_DB_PATH
+
+    with open(db_path, encoding="utf-8", newline="") as city_db:
         csv_reader = csv.reader(city_db, delimiter=";", strict=True)
         next(csv_reader)# skip csv structure line
         for data in csv_reader:

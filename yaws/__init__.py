6231b4041cd8589d806a829dabad156f51dcc6fd
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-10-16 17:35:55 +0300

    import submodules, kick version

--- yaws/__init__.py
+++ yaws/__init__.py
@@ -4,4 +4,12 @@ yaws - weather scrapper
 
 __title__ = "yaws"
 __author__ = "Booplicate"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
+
+from . import (
+    city_lookup,
+    export,
+    scrapper,
+    sql_backend,
+    urls
+)
